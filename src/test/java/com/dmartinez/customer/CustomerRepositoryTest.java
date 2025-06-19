3f28bf6f47a54b92136999320b9ba6d91a063e29
Daniela Martinez <daniela@Danielas-Personal-MacBook.local>
2025-06-19 09:18:34 -0500

    added tests to Customer RepositoryTest class

--- src/test/java/com/dmartinez/customer/CustomerRepositoryTest.java
+++ src/test/java/com/dmartinez/customer/CustomerRepositoryTest.java
@@ -8,6 +8,11 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.ApplicationContext;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class CustomerRepositoryTest extends AbstractTestcontainers {
@@ -24,9 +29,56 @@ class CustomerRepositoryTest extends AbstractTestcontainers {
 
     @Test
     void existsCustomerByEmail() {
+        String email = FAKER.internet().safeEmailAddress() + "_" + UUID.randomUUID();
+        Customer customer = new Customer(
+                FAKER.name().fullName(),
+                email,
+                20
+        );
+        customerRepository.save(customer);
+
+        boolean actual = customerRepository.existsCustomerByEmail(email);
+
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void existsCustomerByEmailFailsWhenEmailNotPresent() {
+        String email = FAKER.internet().safeEmailAddress() + "_" + UUID.randomUUID();
+
+        boolean actual = customerRepository.existsCustomerByEmail(email);
+
+        assertThat(actual).isFalse();
     }
 
     @Test
     void existsCustomerById() {
+        String email = FAKER.internet().safeEmailAddress() + "_" + UUID.randomUUID();
+        Customer customer = new Customer(
+                FAKER.name().fullName(),
+                email,
+                20
+        );
+        customerRepository.save(customer);
+
+        Long id = customerRepository.findAll()
+                .stream()
+                .filter(c -> c.getEmail().equals(email))
+                .map(Customer::getId)
+                .findFirst()
+                .orElseThrow();
+
+        boolean actual = customerRepository.existsCustomerById(id);
+
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void existsCustomerByIdFailsWhenIdNotPresent() {
+         Long id = -1L;
+
+        boolean actual = customerRepository.existsCustomerById(id);
+
+        assertThat(actual).isFalse();
     }
 }
